5db25f974853dca230707a12bce23bccc77c8355
nothappenhere <muhammad.rizky15@mhs.itenas.ac.id>
2025-06-12 21:45:28 +0700

    Test ci/cd #13

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import { useUserStore } from '@/stores/user'
 
-const API_BASE = `http://13.215.172.125/api/auth`
+const API_BASE = import.meta.env.VITE_API_BASE
+  ? `${import.meta.env.VITE_API_BASE}/auth`
+  : 'http://localhost:8000/api/auth'
 
 export const loginUser = async (payload) => {
   const response = await axios.post(`${API_BASE}/login`, payload)
